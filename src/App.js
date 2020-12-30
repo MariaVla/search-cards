d90494a60074dde1e6c009d9770676f4ed3055a7
Majo Vlasanovich <majo@vlasanovich.com>
2020-12-30 08:15:12 -0300

    List cards from an array. Add tachyons for styles.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { robots } from './robots';
+import CardList from './CardList';
 
 function App() {
+  console.log(robots);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Search Cards</h1>
+
+      <CardList cards={robots} />
     </div>
   );
 }
---
+++ src/CardList.js
@@ -0,0 +1,21 @@
+import Card from './Card';
+
+const CardList = ({ cards }) => {
+  return (
+    <div>
+      {cards &&
+        cards.map((card) => {
+          return (
+            <Card
+              key={card.id}
+              id={card.id}
+              name={card.name}
+              email={card.email}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+export default CardList;
