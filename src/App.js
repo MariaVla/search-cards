3e7a52649b980be456a9781804f6320a96b75ef3
Majo Vlasanovich <majo@vlasanovich.com>
2021-01-05 06:48:35 -0300

    Remove useCallback from filteredCards

--- src/App.js
+++ src/App.js
@@ -10,19 +10,23 @@ function App() {
 
   // useCallback will return a memoized version of the callback that only
   // changes if one of the dependencies has changed.
-  const filteredCards = useCallback(
-    () =>
-      cards.filter((card) => {
-        return card.name.toLowerCase().includes(searchField.toLowerCase());
-      }),
-    [JSON.stringify(cards), searchField]
-  );
+  // const filteredCards = useCallback(
+  //   () =>
+  //     cards.filter((card) => {
+  //       return card.name.toLowerCase().includes(searchField.toLowerCase());
+  //     }),
+  //   [JSON.stringify(cards), searchField]
+  // );
+
+  const filteredCards = cards.filter((card) => {
+    return card.name.toLowerCase().includes(searchField.toLowerCase());
+  });
 
   return (
     <div className='tc'>
       <h1>Search Cards</h1>
       <SearchBox onSearchChange={setSearchField} />
-      <CardList cards={filteredCards()} />
+      <CardList cards={filteredCards} />
     </div>
   );
 }
